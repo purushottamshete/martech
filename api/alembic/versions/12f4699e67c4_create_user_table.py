f32542b5f35ad2c6e68352edc205c40caa36fa7e
Purushottam <purushottamshete@gmail.com>
2023-09-26 16:10:25 +0530

    Adding Users Table

---
+++ api/alembic/versions/12f4699e67c4_create_user_table.py
@@ -0,0 +1,61 @@
+"""Create User Table
+
+Revision ID: 12f4699e67c4
+Revises: 
+Create Date: 2023-09-26 15:01:08.385738
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+import pytz
+
+# revision identifiers, used by Alembic.
+revision: str = '12f4699e67c4'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    users_table = op.create_table('users',
+        sa.Column('id', UUID(), primary_key=True, nullable=False),
+        sa.Column('first_name', sa.String(length=30), nullable=False),
+        sa.Column('last_name', sa.String(length=30), nullable=False),
+        sa.Column('email', sa.String(), nullable=False),
+        sa.Column('hashed_password', sa.String(), nullable=False),
+        sa.Column('phone_no', sa.String(length=15), nullable=False),
+        sa.Column('is_active', sa.Boolean(), default=False),
+        sa.Column('last_login', sa.DateTime(timezone=True), nullable=True),
+        sa.Column('language', sa.String(length=2), nullable=False),
+        sa.Column('timezone', sa.String(30), nullable=False),
+        sa.Column('created_at', sa.DateTime, server_default=func.now()),
+        sa.Column('updated_at', sa.DateTime,onupdate=func.now()),
+    )
+    op.create_index(op.f('ix_users_id'), 'users', ['id'], unique=True)
+    op.bulk_insert(
+        users_table,
+        [
+            {
+                "id": uuid.uuid4(),
+                "first_name": "Super",
+                "last_name": "Admin",
+                "email": "contact@martech",
+                "hashed_password": "Admin@123",
+                "phone_no": "9999911111",
+                "is_active": True,
+                "language": 'EN',
+                "timezone": pytz.country_timezones['IT'][0]
+            },
+
+        ],
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(op.f('ix_users_id'), table_name='users')
+    op.drop_table('users')
--- api/models.py
+++ api/models.py
@@ -11,24 +11,24 @@ class Base(DeclarativeBase):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
-class ROLES(str, enum):
-    SUPERADMIN = 'SUPERADMIN',
-    ADMNIN = 'ADMIN',
-    USER = 'USER',
+class ROLES(str, enum.Enum):
+    SUPERADMIN = 'SUPERADMIN'
+    ADMNIN = 'ADMIN'
+    USER = 'USER'
 
 class User(Base):
     __tablename__ = "users"
-    id: Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
-    first_name: Column(String(30))
-    last_name: Column(String(30))
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    first_name = Column(String(30))
+    last_name = Column(String(30))
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     phone_no = Column(String(15))
     is_active = Column(Boolean, default=False)
     last_login = Column(DateTime(timezone=True))
-    language: Column(String(2))
-    timezone: Column(String(30))
-    role: Column(Enum(ROLES), default=ROLES.USER)
+    language = Column(String(2))
+    timezone = Column(String(30))
+    role = Column(Enum(ROLES), default=ROLES.USER)
     # TODO Permissions/Groups
     
 
